a9d5635f06539b876db35a109630588998bc1029
hendrikBischoping <h.bischoping@gmx.de>
2024-11-18 16:20:07 +0100

    first commit

---
+++ json.js
@@ -0,0 +1,117 @@
+let pizzas = [
+    {
+        'name': 'Pizza Magerita',
+        'size1': 'small',
+        'size2': 'medium',
+        'size3': 'large',
+        'price1': 10.99,
+        'price1': 12.99,
+        'price1': 16.99,
+    },
+    {
+        'name': 'Pizza Salami',
+        'size1': 'small',
+        'size2': 'medium',
+        'size3': 'large',
+        'price1': 12.99,
+        'price1': 15.99,
+        'price1': 19.99,
+    },
+    {
+        'name': 'Pizza Hawaii',
+        'size1': 'small',
+        'size2': 'medium',
+        'size3': 'large',
+        'price1': 13.99,
+        'price1': 16.99,
+        'price1': 21.99,
+    },
+    {
+        'name': 'Pizza Diavolo',
+        'size1': 'small',
+        'size2': 'medium',
+        'size3': 'large',
+        'price1': 12.99,
+        'price1': 15.99,
+        'price1': 19.99,
+    },
+    {
+        'name': 'Pizza Kalzone',
+        'size1': 'small',
+        'size2': 'medium',
+        'size3': 'large',
+        'price1': 12.99,
+        'price1': 15.99,
+        'price1': 19.99,
+    },
+    {
+        'name': 'Pizza Tonno',
+        'size1': 'small',
+        'size2': 'medium',
+        'size3': 'large',
+        'price1': 13.99,
+        'price1': 16.99,
+        'price1': 20.99,
+    },
+    
+    {
+        'name': 'Pizza Funghi',
+        'size1': 'small',
+        'size2': 'medium',
+        'size3': 'large',
+        'price1': 11.99,
+        'price1': 14.99,
+        'price1': 18.99,
+    }
+]
+
+let pasta = [
+    {
+        
+    }
+]
+
+let salats = [
+    {
+        'name': 'Feldsalat mit Tomaten und Joghurtdressung',
+        'size1': 'small',
+        'size2': 'medium',
+        'price1': 3.99,
+        'price1': 5.99
+    },
+    {
+        'name': 'Eisbergsalat mit oliven und hauseigened Dressing',
+        'size1': 'small',
+        'size2': 'medium',
+        'price1': 4.99,
+        'price1': 6.99
+    },
+    {
+        'name': 'Weiskrautsalat mit Tomaten, Pepperoni, Kidneynohnen und hauseigened Dressing',
+        'size1': 'small',
+        'size2': 'medium',
+        'price1': 6.99,
+        'price1': 9.99
+    },
+]
+
+let supplements = [
+    {
+        'name': 'Pizzabrötchen normal',
+        'quantity1': 6,
+        'quantity1': 9,
+        'quantity1': 12,
+        'price1': 3.99,
+        'price1': 4.99,
+        'price1': 5.99,
+    },
+    {
+        'name': 'Pizzabrötchen gefüllt mit Käse',
+        'quantity1': 6,
+        'quantity1': 9,
+        'quantity1': 12,
+        'price1': 8.99,
+        'price1': 11.99,
+        'price1': 14.99,
+    }
+]
